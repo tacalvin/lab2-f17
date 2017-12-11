7ffee255fb6888901d3e7834478738999be051f9
Calvin-Khang Ta <cta003@sledge.cs.ucr.edu>
2017-12-10 19:24:33 -0800

    Lab2 pt2

--- shm.c
+++ shm.c
@@ -31,7 +31,49 @@ void shminit() {
 int shm_open(int id, char **pointer) {
 
 //you write this
+	struct proc *curr = myproc();
+	int i;
+	char *va;
+	va = (char*)PGROUNDUP(curr->sz);
 
+	acquire(&(shm_table.lock));
+	for(i = 0; i < 64; ++i)
+	{
+		if(shm_table.shm_pages[i].id == id)
+		{
+			shm_table.shm_pages[i].refcnt += 1;
+			//get curr proc
+			mappages(curr->pgdir, va, PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
+			curr->sz = (uint) va;
+			*pointer = (char*)va;
+			int cnt = shm_table.shm_pages[i].refcnt;
+			release(&(shm_table.lock));
+			return cnt;
+		}
+	}
+
+	// not found time to allocate
+	
+	for(i = 0; i < 64; ++i)
+	{
+		if(shm_table.shm_pages[i].id == 0)
+		{
+			struct shm_page *page = shm_table.shm_pages + i;
+			page->id = id;
+			page->frame = kalloc();
+			memset(page->frame,0,PGSIZE);
+			page->refcnt += 1;
+			mappages(curr->pgdir, va, PGSIZE, V2P(page->frame), PTE_W|PTE_U);
+			*pointer = (char *) (va);
+			curr->sz = (uint) va;
+			page->refcnt += 1;
+			int cnt = page->refcnt;
+			release(&(shm_table.lock));
+			return cnt;
+		}
+	}
+	
+	release(&(shm_table.lock));
 
 
 
@@ -42,7 +84,22 @@ return 0; //added to remove compiler warning -- you should decide what to return
 int shm_close(int id) {
 //you write this too!
 
-
+	int i;
+	acquire(&(shm_table.lock));
+	for(i = 0; i < 64; ++i)
+	{
+		if(shm_table.shm_pages[i].id == id)
+		{
+			shm_table.shm_pages[i].refcnt -= 1;
+			if(!shm_table.shm_pages[i].refcnt)
+			{
+				shm_table.shm_pages[i].id = 0;
+				shm_table.shm_pages[i].refcnt = 0;
+				shm_table.shm_pages[i].frame = 0;
+			}	
+		}
+	}
+	release(&(shm_table.lock));
 
 
 return 0; //added to remove compiler warning -- you should decide what to return
